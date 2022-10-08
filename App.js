3892682a05d505c873de0e23d49b30fa7e2ee5e9
yolla cornelya <yollacornelya454@gmail.com>
2022-10-08 19:54:24 +0700

    merubah App.js

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Router from "./App/Routes";
+import { NavigationContainer } from "@react-navigation/native";
+import { onLoadFont } from "./App/assets/font";
 
-export default function App() {
+export default App = () => {
+  const isLoad = onLoadFont();
+  if (!isLoad) {
+    return null;
+  } 
+
   return (
-    <View style={styles.container}>
-      <Text>Silahkan rubah disini</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
